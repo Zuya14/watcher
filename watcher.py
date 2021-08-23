c5d34d77cb5f2fceb9d62cca589e18c02769af40
Zuya14 <s.ishizuya@gmail.com>
2021-08-24 02:46:39 +0900

    exe

--- watcher.py
+++ watcher.py
@@ -4,12 +4,9 @@ from watchdog.observers import Observer
 import os
 import time
 import csv
-import numpy as np
 import shutil
 
-# PatternMatchingEventHandler の継承クラスを作成
 class FileChangeHandler(PatternMatchingEventHandler):
-    # クラス初期化
     def __init__(self, patterns, copy_path, degs):
         super(FileChangeHandler, self).__init__(patterns=patterns)
         self.copy_path = copy_path
@@ -20,28 +17,24 @@ class FileChangeHandler(PatternMatchingEventHandler):
         for p in patterns:
             self.index[p] = 0
 
-    # ファイル作成時のイベント
     def on_created(self, event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
-        print('%s created' % filename)
+        # print('%s created' % filename)
         self.copy_file(event.src_path)
 
-    # ファイル変更時のイベント
     def on_modified(self, event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
-        print('%s changed' % filename)
+        # print('%s changed' % filename)
         self.copy_file(event.src_path)
 
-    # ファイル削除時のイベント
     def on_deleted(self, event):
         # filepath = event.src_path
         # filename = os.path.basename(filepath)
         # print('%s deleted' % filename)
         pass
 
-    # ファイル移動時のイベント
     def on_moved(self, event):
         # filepath = event.src_path
         # filename = os.path.basename(filepath)
@@ -49,12 +42,16 @@ class FileChangeHandler(PatternMatchingEventHandler):
         pass
 
     def copy_file(self, src_path):
-        print("copy")
+        # print("copy")
         if self.index[src_path] < len(degs):
-            path_deg = (self.copy_path+'/{:.2f}'.format(self.degs[self.index[src_path]]))
+            path_deg = (self.copy_path+'/{:d}'.format(self.degs[self.index[src_path]]))
             if not os.path.exists(path_deg):
                 os.makedirs(path_deg)
-            shutil.copyfile(src_path, path_deg+'/{:s}'.format(os.path.basename(src_path)))
+            
+            save_path = path_deg+'/{:s}'.format(os.path.basename(src_path))
+            shutil.copyfile(src_path, save_path)
+
+            print("save: {:s}".format(save_path))
 
             self.index[src_path] += 1
 
@@ -72,12 +69,16 @@ if __name__ == "__main__":
             if row[0] == 'copy_path':
                 param[row[0]] = row[1]
             else:
-                param[row[0]] = float(row[1])
+                param[row[0]] = int(row[1])
 
-    degs = np.arange(param['start_deg'], param['end_deg']+1e-6, param['deg_step'])
+    degs = list(range(param['start_deg'], param['end_deg']+1, param['deg_step']))
+
+    print("read parameter.csv")
 
     with open('./file_path_list.txt') as f:
         file_paths = [l.replace("\n", "") for l in f.readlines()]
+
+    print("read file_path_list.txt")
 
     f = file_paths[0]
 
@@ -86,6 +87,8 @@ if __name__ == "__main__":
     observer.schedule(event_handler, os.path.dirname(file_paths[0]), recursive=True)
     observer.start()
 
+    print("watching...")
+    
     try:
         while True:
             time.sleep(0.1)
